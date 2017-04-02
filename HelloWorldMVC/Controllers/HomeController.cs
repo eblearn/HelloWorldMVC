990dcc15bfe76bbc9b752739963399b6a749851c
Éric Bélanger <ericbelanger28@hotmail.com>
2017-04-02 18:27:24 -0400

    My commit of the project
    Has all default files created by VS project creation wizard

---
+++ HelloWorldMVC/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace HelloWorldMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Ma première appli web en MVC :-)";
+            return View();
+        }
+    }
+}
